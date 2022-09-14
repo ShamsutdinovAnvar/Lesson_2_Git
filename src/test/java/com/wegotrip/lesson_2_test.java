c326754a78293187dc0631b3b3b88a299077105c
ShamsutdinovAnvar <a.shamsutdinov2021@gmail.com>
2022-09-14 22:11:03 +0300

    Init commit from console

---
+++ src/test/java/com/wegotrip/lesson_2_test.java
@@ -0,0 +1,16 @@
+package com.wegotrip;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class lesson_2_test {
+    @Test
+    void exampleTest0() {
+        Assertions.assertTrue(2 > 1);
+    }
+
+    @Test
+    void exampleTest1() {
+        Assertions.assertTrue(3 > 2);
+    }
+}
