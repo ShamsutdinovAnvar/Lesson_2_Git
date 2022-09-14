48da82ee6ce1a166ca53dccbb072b55537cce804
ShamsutdinovAnvar <a.shamsutdinov2021@gmail.com>
2022-09-14 23:06:36 +0300

    this is real true

--- src/test/java/com/wegotrip/lesson_2_test.java
+++ src/test/java/com/wegotrip/lesson_2_test.java
@@ -6,15 +6,15 @@ import org.junit.jupiter.api.Test;
 public class lesson_2_test {
     @Test
     void exampleTest0() {
-        Assertions.assertTrue(2 > 1);
+        Assertions.assertTrue(12 > 1);
     }
 
     @Test
     void exampleTest1() {
-        Assertions.assertTrue(3 > 2);
+        Assertions.assertTrue(43 > 2);
     }
     @Test
     void exampleTest2() {
-        Assertions.assertTrue(4 > 3);
+        Assertions.assertTrue(74 > 3);
     }
 }
